33b09df29164fbd90cac6f81fdc9a135ebe86a5e
amaniesseili <esseiliamani@hotmail.com>
2023-07-07 19:12:09 +0200

    array e somma degli elementi

---
+++ main.js
@@ -0,0 +1,27 @@
+/*Snack 1
+Crea un array vuoto -------> array[]
+e chiedi all’utente un numero da inserire nell’array. --------> prompt
+Continua a chiedere i numeri all’utente e a inserirli nell’array -------> const inputnumber -----> conversione dell input in un numero intero tramite parseInt(input)
+fino a quando la somma degli elementi è minore di 50. --------> while(la somma < 50)... ( + let somma/sum ) + if..*/ 
+
+
+const arrayVuota = [ ];
+// const inputNumber = prompt("Inserici un numero da inserire nell'array : ");
+let sum = 0; 
+
+const num = parseInt(inputNumber);
+
+while ( sum < 50){
+    const inputNumber = prompt("Inserici un numero da inserire nell'array : ");
+
+    if (! isNaN(num)){    // 1- verifica se l'input è un numero valido allora 
+        Array.push(num);  // 2- continua a pushare i numeri finchè  
+
+        sum += num;       // 3- la somma dei num>50 
+
+    } else{
+        alert("Inserisci un numero valido.")  // 4-altirmenti se l'input nn è un numero valido ----> alert messaggio errore l'input nn e un numero
+    }
+}
+
+console.log ("array")
